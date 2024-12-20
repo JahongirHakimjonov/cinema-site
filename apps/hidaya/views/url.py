439d9a1067c6665e23cf6f30ddb29165edaaaa5c
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2024-12-20 11:16:55 +0500

    fix: url hashed edit

--- apps/hidaya/views/url.py
+++ apps/hidaya/views/url.py
@@ -1,23 +1,20 @@
 import hashlib
-
+from django.utils import timezone
 from django.http import JsonResponse
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from apps.hidaya.utils import generate_signed_url
-from core import settings
-
-my_key = settings.SECRET_KEY
 
 
 class GetSignedVideoURLView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request, video_id, quality, file_name):
+    def post(self, request, video_id, quality, file_name):
         key = request.data.get("key")
         if not key:
             return JsonResponse({"error": "Key is required"}, status=400)
-        combined_key = f"{my_key}{video_id}{quality}{file_name}"
+        combined_key = f"{video_id}{quality}{file_name}"
         hashed_key = hashlib.sha256(combined_key.encode()).hexdigest()
         if key != hashed_key:
             return JsonResponse({"error": "Invalid key"}, status=400)
--- hash256.py
+++ hash256.py
@@ -6,18 +6,17 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(".env"))
 
-key = os.getenv("SECRET_KEY")
 video_id = "3"
 quality = "1080p"
 file_name = "playlist.m3u8"
 
-combined_key = f"{key}{video_id}{quality}{file_name}"
+combined_key = f"{video_id}{quality}{file_name}"
 hashed_key = hashlib.sha256(combined_key.encode()).hexdigest()
 
 print(hashed_key)
 
 url = f"http://127.0.0.1:8003/api/v1/videos/{video_id}/{quality}/{file_name}/signed-url/"
 
-response = requests.get(url, data={"key": hashed_key})
+response = requests.post(url, data={"key": hashed_key})
 
 print(response.json())
